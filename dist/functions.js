053738ca6bfd4fb42bd47b16a50f47cc45cc0b39
Gaute Meek Olsen <gaute.meek@gmail.com>
2018-11-21 21:30:31 +0100

    functional programming

--- dist/functions.js
+++ dist/functions.js
@@ -4,3 +4,10 @@ const buildName = (firstName, lastName) => {
 };
 const myName = buildName('Gaute', 'Meek Olsen');
 const noldus = buildName('Noldus');
+/* Functional programming */
+function func(method) {
+    method();
+    return method;
+}
+const sayHi = func(() => console.log('Hi'));
+sayHi(); //sayHi is called insided func and afterwards
--- src/functions.ts
+++ src/functions.ts
@@ -4,3 +4,12 @@ const buildName = (firstName: string, lastName?: string) => {
 
 const myName = buildName('Gaute', 'Meek Olsen');
 const noldus = buildName('Noldus');
+
+/* Functional programming */
+function func(method: Function):Function{
+    method();
+    return method;
+}
+
+const sayHi = func(() => console.log('Hi'));
+sayHi(); //sayHi is called insided func and afterwards
